3d232470e2e686646bb12d6372666399c8692745
Júnior Bocelli <juniorbocelli@gmail.com>
2023-10-16 14:55:38 -0300

    .

--- src/config-global.ts
+++ src/config-global.ts
@@ -1,5 +1,5 @@
 // Google
-export const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY
+export const GOOGLE_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_API_KEY
 
 // Layout
 export const HEADER_HEIGHT = 80;
